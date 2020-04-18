f93d68f5d5028d1891f801458bcc3e6285a46c0e
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-18 17:11:21 +0700

    Chapter 3: Add shoulda matchers for user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe User, type: :model do
 
   let(:user) { build(:user) }
 
-  it { is_expected.to respond_to(:email) }
+  it { should respond_to(:email) }
   it { is_expected.to respond_to(:password) }
   it { is_expected.to respond_to(:password_confirmation) }
 
   it { is_expected.to be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
 end
