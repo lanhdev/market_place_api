628feeeee9e5eae7fa5c137a8a400fabc651f1b4
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-18 16:45:07 +0700

    Chapter 3: Add user first spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { user }
+
+  let(:user) { build(:user) }
+
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+
+  it { is_expected.to be_valid }
 end
