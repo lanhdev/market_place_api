e12ec9add2866c54764e5ea61e66261b479ddf66
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-19 15:28:44 +0700

    Chapter 5: Add SessionsController create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: []
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: %i(show create update destroy)
+      resources :sessions, only: %i(create destroy)
     end
   end
 end
