20c4f05f0112c1df074a0d4256f7378f8d6ca1d7
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-19 14:52:40 +0700

    Chapter 5: Add generate_authentication_token! method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe User, type: :model do
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   it { should validate_uniqueness_of(:auth_token) }
+
+  describe '#generate_authentication_token!' do
+    let(:existing_user) { create(:user, auth_token: 'auniquetoken123') }
+
+    it 'generates a unique token' do
+      allow(Devise).to receive(:friendly_token).and_return('auniquetoken123')
+      user.generate_authentication_token!
+      expect(user.auth_token).to eq 'auniquetoken123'
+    end
+
+    it 'generates another token when one already has been taken' do
+      user.generate_authentication_token!
+      expect(user.auth_token).not_to eq existing_user.auth_token
+    end
+  end
 end
