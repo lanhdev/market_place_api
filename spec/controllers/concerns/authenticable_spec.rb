454590062b6f805be9c0576f0db2656a9451ebf4
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-19 17:56:22 +0700

    Chapter 5: Add user_signed_in? method to know whether the user is logged in or not

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -41,4 +41,24 @@ describe Authenticable do
 
     it { should respond_with 401 }
   end
+
+  describe '#user_signed_in?' do
+    context "when there is a user on 'session'" do
+      let(:user) { create(:user) }
+
+      before do
+        allow(authentication).to receive(:current_user).and_return(user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "when there is no user on 'session'" do
+      before do
+        allow(authentication).to receive(:current_user).and_return(nil)
+      end
+
+      it { should_not be_user_signed_in }
+    end
+  end
 end
