6acc58ae6322497b12f7a0643947e5535a62ae05
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-19 11:20:49 +0700

    Chapter 4: Refactor test headers for each request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,11 @@ end
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Request::JsonHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
