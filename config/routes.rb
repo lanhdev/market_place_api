7405bce8da9aa366f14ab5f40af54914d02892d0
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-20 22:58:57 +0700

    Chapter 6: Add create action for ProductsController

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, only: []
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: %i(show create update destroy)
+      resources :users, only: %i(show create update destroy) do
+        resources :products, only: %i(create)
+      end
       resources :sessions, only: %i(create destroy)
       resources :products, only: %i(index show)
     end
