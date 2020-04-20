0d645ce7549ab72f05f7bea35d098f37a5396d8a
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-20 21:14:18 +0700

    Chapter 6: Add some validations to Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,4 +11,9 @@ RSpec.describe Product, type: :model do
   it { is_expected.to respond_to(:user_id) }
 
   it { is_expected.not_to be_published }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of(:user_id) }
 end
