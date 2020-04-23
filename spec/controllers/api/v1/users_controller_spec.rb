becbf57c2d55d365ad7be109e1a71c96d8d35e84
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-23 13:36:34 +0700

    Chapter 7: Add UserSerializer for customizing json output

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     it 'returns the information about a reporter on a hash' do
-      user_response = json_response
+      user_response = json_response[:user]
       expect(user_response[:email]).to eq @user.email
     end
 
@@ -23,7 +23,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders the json representation for the user record just created' do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eq @user_attributes[:email]
       end
 
@@ -69,7 +69,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it 'renders the json representation for the updated user' do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eq 'newmail@example.com'
       end
 
