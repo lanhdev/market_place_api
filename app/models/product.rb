cb00b3046b3a16cdac8bbeb9a02b15868e6e202a
Lanh Hoang <lanhhoang@users.noreply.github.com>
2020-04-23 16:13:54 +0700

    Chapter 7: Add filter_by_title scope to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
 
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+  scope :filter_by_title, ->(keyword) do
+    where('lower(title) ILIKE ?', "%#{keyword.downcase}%")
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:price) }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of(:user_id) }
+
+  describe '.filter_by_title' do
+    let!(:product1) { create(:product, title: 'A plasma TV') }
+    let!(:product2) { create(:product, title: 'Fastest laptop') }
+    let!(:product3) { create(:product, title: 'CD player') }
+    let!(:product4) { create(:product, title: 'LCD TV') }
+
+    context "when a 'TV' title pattern is sent" do
+      it 'returns 2 products matching' do
+        expect(Product.filter_by_title('TV').size).to eq 2
+      end
+
+      it 'returns products matching' do
+        expect(Product.filter_by_title('TV').sort).to match_array([product1, product4])
+      end
+    end
+  end
 end
